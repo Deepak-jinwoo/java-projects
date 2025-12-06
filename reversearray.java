40e9ccb0e56fd1b014e66c237d990ece731fc3ce
Deepak <msdeepak311@gmail.com>
2025-12-06 20:10:26 +0530

    Add files via upload

--- reversearray.java
+++ reversearray.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-public class reversearray{
+class reversearray{
     public static void main(String[] args) {
     Scanner scanner=new Scanner(System.in);
     System.out.println("enter how many nums sh");
     int n = scanner.nextInt();
-    int []arr=new int[n];
-    System.out.println("enter nums");
+    int []arr=new int[n]; 
+     System.out.println("enter nums");  
     for(int i = 0 ; i < arr.length ; i++){
-       
       arr[i]=scanner.nextInt();
     }
-    for(int j=n-1 ; j>=40 ; j--){
-       System.out.println(arr[j]);
+    System.out.println("enter number to search");
+    int num=scanner.nextInt();
+    for(int j=0;j<arr.length;j++){
+      if (arr[j]==num) {
+        System.out.println("the num found at index"+j);
+        
+      }   
     }
 }
 }
